05dec2affeba9568a92bdbed46b5cfd2852f4813
CARLOS MARIO RESTREPO <cmrestrepoc@unal.edu.co>
2023-03-31 19:45:33 -0500

    scripts setup and routes to start

---
+++ src/components/Login.js
@@ -0,0 +1,7 @@
+function Login() {
+    return (
+        <h1>Login View Here</h1>
+    )
+}
+
+export default Login
---
+++ src/components/MainMenu.js
@@ -0,0 +1,7 @@
+function MainMenu() {
+    return (
+        <h1>MainMenu View Here</h1>
+    )
+}
+
+export default MainMenu
